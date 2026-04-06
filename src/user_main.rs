c971bd9fa414b9565c6f1b02fe858995519b287e
Vasyl-Trefilov <drenfor228@gmail.com>
2026-04-06 16:17:13 +0200

    Apply cargo fix to remove unused imports

--- src/user_main.rs
+++ src/user_main.rs
@@ -2,11 +2,7 @@
 
 //! This is a test file, where I test the user-friendliness of my library
 mod examples;
-use crate::examples::physics::main as physics_main;
-use crate::examples::rotate::main as rotate_main;
-use crate::examples::shaders::main as shaders_main;
 use crate::examples::stress_pbr::main as pbr_main;
-use crate::examples::stress_unlit::main as unlit_main;
 
 fn main() {
     // Comment out the function you don't want to run, and uncomment the one you do!
